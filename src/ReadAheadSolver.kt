666565a95928d7ab6d04e396603e02129c913839
NakagamiYuta <guguru0014@gmail.com>
2018-10-09 20:59:11 +0900

    [add]先読みソルバーをとりあえず作成

---
+++ src/ReadAheadSolver.kt
@@ -0,0 +1,22 @@
+import java.util.Random;
+
+val random = Random()
+
+fun main(args: Array<String>){
+    val agentPos = getAgentPosFromQR()
+    val scoreData = getScoreDataFromQR()
+    val megurimasu = MegurimasuSimulator(agentPos, scoreData)
+}
+
+fun getAgentPosFromQR(): Map<String, Array<Int>>{
+    return mapOf(
+            "A_1" to arrayOf(0, 1),
+            "A_2" to arrayOf(0, 2),
+            "B_1" to arrayOf(0, 3),
+            "B_2" to arrayOf(0, 4)
+    )
+}
+
+fun getScoreDataFromQR(): Array<Array<Int>>{
+    return Array(12) { _ -> Array(12) {random.nextInt(32)-16}}
+}
