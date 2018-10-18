cdd9dc9616e9e7fbc6c888cff26bc28c241ef9d6
NakagamiYuta <guguru0014@gmail.com>
2018-10-18 15:38:19 +0900

    [change]whileループの間隔を変更
    ・Thread.sleep(100) -> Thread.sleep(5)

--- src/MegurimasuMain.kt
+++ src/MegurimasuMain.kt
@@ -8,10 +8,8 @@ fun main(args: Array<String>){
 
     // QRデータ待機
     println("Please Input QR Data")
-//    while(qrData == null){ Thread.sleep(100) }
+    while(qrData == null){ Thread.sleep(5) }
     println("Received QR Data")
-
-    qrData = "12 9:13 14 12 6 14 6 12 14 13:2 0 10 13 15 13 10 0 2:7 10 14 0 3 0 14 10 7:4 13 14 11 6 11 14 13 4:8 7 0 6 2 6 0 7 8:8 4 9 11 8 11 9 4 8:8 4 9 11 8 11 9 4 8:8 7 0 6 2 6 0 7 8:4 13 14 11 6 11 14 13 4:7 10 14 0 3 0 14 10 7:2 0 10 13 15 13 10 0 2:13 14 12 6 14 6 12 14 13:5 3:8 7:"
 
     // スコアデータとエージェント初期位置取得
     val qrParser = QRParser(qrData!!)
