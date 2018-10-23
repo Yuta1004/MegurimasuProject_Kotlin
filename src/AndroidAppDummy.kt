859762913c1c62f398ab19711b3be5084040e36e
NakagamiYuta <guguru0014@gmail.com>
2018-10-23 21:11:58 +0900

    [add]AndroidAppが使えない時用のファイルを作成

---
+++ src/AndroidAppDummy.kt
@@ -0,0 +1,19 @@
+import java.net.ServerSocket
+
+fun main(args: Array<String>) {
+    val server = ServerSocket(6666)
+    val socket = server.accept()
+
+    while(true) {
+        print("Please Input Send Text > ")
+        val text = readLine()
+
+        try {
+            val writer = socket!!.getOutputStream()
+            writer.write((text + "\n").toByteArray())
+        } catch (e: Exception) {
+            socket.close()
+            server.close()
+        }
+    }
+}
