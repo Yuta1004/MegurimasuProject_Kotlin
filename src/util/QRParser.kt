cbed27aaea6ebd0e5e0ccaf464d2a191f462175c
NakagamiYuta <guguru0014@gmail.com>
2018-10-27 22:15:04 +0900

    [hothothothothothotfix]QRデータないのエージェント情報を読み込む処理に重大なバグがあったので修正

--- src/util/QRParser.kt
+++ src/util/QRParser.kt
@@ -4,6 +4,7 @@ class QRParser(qrText: String){
     private val qrData = qrText.split(":")
     private val stageSizeInfo = qrData[0].split(" ")
     private val height = stageSizeInfo[0].toInt()
+    private val width = stageSizeInfo[1].toInt()
 
     fun getAgentPos(): Map<String, Array<Int>>{
         // わかりやすさを優先してループを使わない
@@ -15,12 +16,26 @@ class QRParser(qrText: String){
         val agentA2PosY = agentA2Pos[0].toInt() - 1
         val agentA2PosX = agentA2Pos[1].toInt() - 1
 
-        return mapOf(
-                "A_1" to arrayOf(agentA1PosX, agentA1PosY),
-                "A_2" to arrayOf(agentA2PosX, agentA2PosY),
-                "B_1" to arrayOf(agentA1PosX, agentA2PosY),
-                "B_2" to arrayOf(agentA2PosX, agentA1PosY)
-        )
+        when {
+            agentA1PosX == agentA2PosX -> return mapOf(
+                    "A_1" to arrayOf(agentA1PosX, agentA1PosY),
+                    "A_2" to arrayOf(agentA2PosX, agentA2PosY),
+                    "B_1" to arrayOf(width-agentA1PosX-1, agentA1PosY),
+                    "B_2" to arrayOf(width-agentA2PosX-1, agentA2PosY)
+            )
+            agentA1PosY == agentA2PosY -> return mapOf(
+                    "A_1" to arrayOf(agentA1PosX, agentA1PosY),
+                    "A_2" to arrayOf(agentA2PosX, agentA2PosY),
+                    "B_1" to arrayOf(agentA1PosX, height-agentA1PosY-1),
+                    "B_2" to arrayOf(agentA2PosX, height-agentA2PosY-1)
+            )
+            else -> return mapOf(
+                    "A_1" to arrayOf(agentA1PosX, agentA1PosY),
+                    "A_2" to arrayOf(agentA2PosX, agentA2PosY),
+                    "B_1" to arrayOf(agentA1PosX, agentA2PosY),
+                    "B_2" to arrayOf(agentA2PosX, agentA1PosY)
+            )
+        }
     }
 
     fun getScoreData(): Array<Array<Int>>{
